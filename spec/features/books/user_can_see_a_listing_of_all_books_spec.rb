8bd60d4648a842e0bf90495d20945eab669f847b
Brandon Stewart <boveus@gmail.com>
2017-09-25 17:53:21 -0600

    Add initial passing test.

--- spec/features/books/user_can_see_a_listing_of_all_books_spec.rb
+++ spec/features/books/user_can_see_a_listing_of_all_books_spec.rb
@@ -1,23 +1,19 @@
 require "rails_helper"
 
 feature "A visitor can see an index of books when they visit the books index" do
-  scenario "They see a list of books" do
+  scenario "They see a list of books when they visit the books index" do
+    5.times do
+      name = Faker::Book.author.split(' ')
+      author = Author.create(first_name: name[0], last_name: name[1])
+      book = Book.create(title: Faker::Book.title, author: author, date_published: Faker::Date.between(90.years.ago, Date.today))
+    end
 
+    visit books_path
 
-    visit item_path(item1)
-
-    click_on "add_shopping_cart"
-    click_on "add_shopping_cart"
-
-    click_on "shopping_cart"
-
-    expect(page).to have_content("2")
-    expect(page).to have_content(100.0)
-
-    click_on "delete"
-
-    expect(page).to have_content("Successfully removed #{item1.name}")
-    expect(current_path).to eq(cart_path)
-    expect(page).to have_content("1")
-    expect(page).to have_content(50.0)
+    expect(page).to have_content("Books index")
+    expect(page).to have_content("#{Book.first.author_name}")
+    expect(page).to have_content("#{Book.second.author_name}")
+    expect(page).to have_content("#{Book.third.date_published}")
+    expect(page).to have_content("#{Book.fourth.title}")
   end
+end
