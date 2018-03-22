0f8f6876630a7f76f51eaa6925f0e535b6a7f66c
Brandon Stewart <bstewart40@gsu.edu>
2018-03-22 10:43:24 -0400

    add coffee-script

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :author
 
+  def bad_method
+    "this is an example of a code smell because this line is far too long.  If a line exceeds 80 characters it is a code smell."
+  end
+
   def author_name
     "#{author.first_name} #{author.last_name}"
   end
